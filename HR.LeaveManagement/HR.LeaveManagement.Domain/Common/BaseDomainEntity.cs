34894a21557514d9e3c50f4504a5bc2d944c83c3
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2024-09-29 00:00:07 +0700

    Create base project

---
+++ HR.LeaveManagement/HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public int ID { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
